d8edb36068979c848268bff479c4ca3df1f77238
blondeman <domo23000@gmail.com>
2025-09-14 20:15:31 -0600

    update function

--- MultiplayerTest/scripts/global/Network.gd
+++ MultiplayerTest/scripts/global/Network.gd
@@ -9,6 +9,7 @@ var lobby_members_max: int = 8
 
 signal lobby_members_updated(lobby_members: Array)
 signal chat_received(username: String, message: String)
+signal update_received(type: String, id: int, data: Dictionary)
 
 func _ready():
 	Steam.lobby_created.connect(_on_lobby_created)
@@ -29,7 +30,7 @@ func _on_lobby_created(connect: int, this_lobby_id: int):
 		Steam.setLobbyJoinable(lobby_id, true)
 		Steam.setLobbyData(lobby_id, "name", "My Lobby")
 		
-		print("lobby created: ", lobby_id)
+		emit_signal("chat_received", "SYSTEM", "lobby created: " + str(lobby_id))
 		
 		var set_relay: bool = Steam.allowP2PPacketRelay(true)
 
@@ -39,8 +40,6 @@ func join_lobby(this_lobby_id: int):
 func _on_lobby_joined(this_lobby_id: int, _permissions: int, _locked: bool, response: int):
 	if response == Steam.CHAT_ROOM_ENTER_RESPONSE_SUCCESS:
 		lobby_id = this_lobby_id
-		
-		print("lobby joined: ", lobby_id)
 		
 		get_lobby_members()
 		send_user_packet("handshake")
@@ -56,32 +55,47 @@ func get_lobby_members():
 	
 	emit_signal("lobby_members_updated", lobby_members)
 
-func send_p2p_packet(this_target: int, packet_data: Dictionary, send_type: int = 0):
+func send_p2p_packet(this_target: int, packet_data: Dictionary, send_type: int = 0) -> bool:
 	var channel: int = 0
 	var this_data: PackedByteArray
 	this_data.append_array(var_to_bytes(packet_data))
 	
-	if this_target == 0:
+	if this_target == 0: 
+		#SEND TO ALL USERS
 		if lobby_members.size():
+			var success = true
 			for member in lobby_members:
 				if member['steam_id'] != Global.steam_id:
-					Steam.sendP2PPacket(member['steam_id'], this_data, send_type, channel)
-	else:
-		Steam.sendP2PPacket(this_target, this_data, send_type, channel)
+					if !Steam.sendP2PPacket(member['steam_id'], this_data, send_type, channel):
+						success = false
+			return success
+		else: 
+			#NO MEMBERS IN LOBBY
+			return false
+	else: 
+		#SEND TO SPECIFIC USER
+		return Steam.sendP2PPacket(this_target, this_data, send_type, channel)
+
+func send_chat(message: String) -> bool:
+	if send_user_packet("chat", {"chat": message}):
+		emit_signal("chat_received", Global.steam_username, message)
+		return true
+	return false
+
+# USAGE
+# Network.send_update("position", {"x": 1, "y":2, "z":3})
+func send_update(type: String, data: Dictionary = {}) -> bool:
+	return send_user_packet("update", {"data": data})
+
+func send_user_packet(type: String, data: Dictionary = {}) -> bool:
+	data['message'] = type
+	data['steam_id'] = Global.steam_id
+	data['steam_username'] = Global.steam_username
+	return send_p2p_packet(0, data)
 
 func _on_p2p_session_request(remote_id: int):
 	var this_requester: String = Steam.getFriendPersonaName(remote_id)
 	Steam.acceptP2PSessionWithUser(remote_id)
-
-func send_chat(message: String):
-	emit_signal("chat_received", Global.steam_username, message)
-	send_user_packet("chat", {"chat": message})
-
-func send_user_packet(message: String, data: Dictionary = {}):
-	data['message'] = message
-	data['steam_id'] = Global.steam_id
-	data['steam_username'] = Global.steam_username
-	send_p2p_packet(0, data)
 
 func read_all_p2p_packets(read_count: int = 0):
 	if read_count > PACKET_READ_LIMIT:
@@ -103,7 +117,9 @@ func read_p2p_packet():
 		if readable_data.has("message"):
 			match readable_data["message"]:
 				"handshake":
-					emit_signal("chat_received", readable_data["steam_username"], " JOINED")
+					emit_signal("chat_received", "SYSTEM", readable_data["steam_username"] + " joined")
 					get_lobby_members()
 				"chat":
 					emit_signal("chat_received", readable_data["steam_username"], readable_data["chat"])
+				"update":
+					emit_signal("update_received", readable_data["type"], readable_data["steam_id"], readable_data["data"])
