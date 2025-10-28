bfaa04037438cc8c7f654c084995edee37ec4ee3
Richard Chavez <124701099+Chavez300@users.noreply.github.com>
2025-10-28 12:16:16 -0600

    Projectile gun (#11)
    * Projectile gun
    
    * changed projectile weapon to work with player
    
    ---------
    
    Co-authored-by: blondeman <domo23000@gmail.com>

---
+++ MultiplayerTest/scripts/player/GunProjectile.gd
@@ -0,0 +1,26 @@
+extends Gun
+class_name GunProjectile
+
+@export var projectile_scene: PackedScene = preload("res://scenes/game/projectile.tscn")
+
+func _ready():
+	super()
+
+func _shoot():
+	var from = global_position
+	var to = get_global_mouse_position()
+	
+	on_shoot(0, {"from": from, "to": to})
+	Multiplayer.update_gun_shoot(from, to)
+
+func on_shoot(_steam_id: int, data: Dictionary):
+	var from = data["from"]
+	var to = data["to"]
+	var shoot_direction = (to - from).normalized()
+	
+	var projectile = projectile_scene.instantiate()
+	projectile.global_position = ray_start.global_position
+	projectile.direction = shoot_direction
+	projectile.gun_owner = self
+
+	get_tree().root.add_child(projectile)
---
+++ MultiplayerTest/scripts/player/Projectile.gd
@@ -0,0 +1,22 @@
+extends Area2D
+class_name Projectile
+
+@export var shoot_velocity := 400.0
+@export var lifetime := 5.0
+
+var direction := Vector2.ZERO
+var velocity := Vector2.ZERO
+var gun_owner: Gun = null
+
+func _ready() -> void:
+	velocity = direction * shoot_velocity
+	await get_tree().create_timer(lifetime).timeout
+	queue_free()
+
+func _process(delta: float) -> void:
+	velocity.y += gravity * delta
+	position += velocity * delta
+
+func _on_body_entered(body: Node) -> void:
+	gun_owner._handle_hit(body, position)
+	queue_free()
